549051442d4ccdd893e93c271853061be3293ccb
Logan Chien <tzuhsiang.chien@gmail.com>
2013-05-29 18:56:46 +0800

    Enable PIC for Android even if "-static" is specified.
    [WORK IN PROGRESS] XXX Missing test cases.
    
    Android only supports a limited subset of the relocation
    types on ARM and MIPS architecture.  Without this fix,
    the Android build system can't link the executable properly.

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -2148,7 +2148,8 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   if (KernelOrKext &&
       (!Triple.isiOS() || Triple.isOSVersionLT(6)))
     PIC = PIE = false;
-  if (Args.hasArg(options::OPT_static))
+  if (Args.hasArg(options::OPT_static) &&
+      getToolChain().getTriple().getEnvironment() != llvm::Triple::Android)
     PIC = PIE = false;
 
   if (Arg *A = Args.getLastArg(options::OPT_mdynamic_no_pic)) {
