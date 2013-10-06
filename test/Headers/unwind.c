b47b758a330ea3d17f0a8d9366f64c7251c763b9
synergydev <synergye@codefi.re>
2013-10-06 15:38:05 -0700

    Revert "Define _Unwind_GetIP() for ARM."
    This reverts commit e9a7a849c8a9f65bcb063f88724e473b513fbed0.

--- test/Headers/unwind.c
+++ test/Headers/unwind.c
@@ -1,23 +1,19 @@
-// RUN: %clang_cc1 -triple arm-unknown-linux-gnueabi -Wall -Werror \
+// RUN: %clang_cc1 -triple arm-unknown-linux-gnueabi \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only %s
-// RUN: %clang_cc1 -triple mips-unknown-linux -Wall -Werror \
+// RUN: %clang_cc1 -triple mips-unknown-linux \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only %s
-// RUN: %clang_cc1 -triple i686-unknown-linux -Wall -Werror \
+// RUN: %clang_cc1 -triple i686-unknown-linux \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only %s
-// RUN: %clang_cc1 -triple x86_64-unknown-linux -Wall -Werror \
+// RUN: %clang_cc1 -triple x86_64-unknown-linux \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only %s
 
-// RUN: %clang_cc1 -triple arm-unknown-linux-gnueabi -Wall -Werror \
+// RUN: %clang_cc1 -triple arm-unknown-linux-gnueabi \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only -x c++ %s
-// RUN: %clang_cc1 -triple mips-unknown-linux -Wall -Werror \
+// RUN: %clang_cc1 -triple mips-unknown-linux \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only -x c++ %s
-// RUN: %clang_cc1 -triple i686-unknown-linux -Wall -Werror \
+// RUN: %clang_cc1 -triple i686-unknown-linux \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only -x c++ %s
-// RUN: %clang_cc1 -triple x86_64-unknown-linux -Wall -Werror \
+// RUN: %clang_cc1 -triple x86_64-unknown-linux \
 // RUN:   -isystem %S/Inputs/include -ffreestanding -fsyntax-only -x c++ %s
 
 #include "unwind.h"
-
-uintptr_t test_unwind_get_ip(struct _Unwind_Context *context) {
-  return _Unwind_GetIP(context);
-}
